f972821567ebfe805e15f6e10ccca00e9d2e77d9
sshakya31397 <sshakya31397@ucumberlands.edu>
2025-01-06 15:16:11 -0500

    Insertion Sort Algorithm

---
+++ MSCS532_Assignment1/algorithm.py
@@ -0,0 +1,22 @@
+def insertion_sort_decreasing(arr):
+    # sort out the values in the arr starting from 1
+    for i in range(1, len(arr)):
+        key = arr[i]  # The current element to be inserted
+        j = i - 1  # Start comparing with the element before it
+        
+        # moving the elements of an array in decreasing order
+        while j >= 0 and arr[j] < key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        
+        # placing the key after the correct position
+        arr[j + 1] = key
+
+    return arr
+
+
+arr = [1, 14, 22, 45, 71, 3, 15]
+print("Original array:", arr)
+
+sorted_arr = insertion_sort_decreasing(arr)
+print("Sorted array in decreasing order:", sorted_arr)
